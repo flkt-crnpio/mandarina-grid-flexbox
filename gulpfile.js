8c8820408183fdeef5af20d29ac1fd9e2e104a01
flkt-crnpio <flkt.crnpio@gmail.com>
2018-08-11 14:33:14 -0500

    Dev dependencies updated

--- gulpfile.js
+++ gulpfile.js
@@ -13,42 +13,39 @@ var gulp = require('gulp'),
         'android 4'
     ];
 
-gulp.task('dist', ['sass-dist']);
+gulp.task('dist', function(done) {
+    return gulp.src(['src/grid.scss'])
+        .pipe(sourcemaps.init())
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(cssnano({ autoprefixer: { browsers: supportedbrowsers, add: true } }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('dist'));
+    done();
+});
 
-gulp.task('dev', ['normalize', 'sass-dev'], function() {
+gulp.task('server', function() {
     browserSync.init({
         server: {
             baseDir: "./dev"
         }
     });
-    gulp.watch("dev/index.html").on('change', browserSync.reload);
-    gulp.watch("src/**/*.scss", ['sass-dev']);
 });
-
-gulp.task('sass-dist', function(){
+gulp.task('sass', function() {
     return gulp.src(['src/grid.scss'])
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(cssnano({
-            autoprefixer: {browsers: supportedbrowsers, add: true}
-        }))
-        .pipe(rename({ extname: '.min.css' }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('dist'))
+        .pipe(sass({ outputStyle: 'expanded' }).on('error', sass.logError))
+        .pipe(gulp.dest('dev/css'))
+        .pipe(browserSync.stream());
 });
-
-gulp.task('sass-grid', function(){
-    return gulp.src(['src/grid.scss'])
-        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-        .pipe(gulp.dest('dev/css'));
-});
-
-gulp.task('sass-dev', ['sass-grid'], function (done) {
-    browserSync.reload();
-    done();
-});
-
 gulp.task('normalize', function() {
     return gulp.src('node_modules/m.normalize/normalize.min.css')
         .pipe(gulp.dest('dev/css/'));
 });
+
+gulp.watch('src/**/*.scss', gulp.series('sass')).on('change', function() {
+  browserSync.reload;
+});
+
+gulp.task('default', gulp.parallel('server', 'normalize', 'sass', function(done) {
+    done();
+}));
